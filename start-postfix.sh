0a978c33a66e4652139e84e68c9e825e409a3663
Maxime Mérian <maxime@merian.me>
2019-09-09 19:19:32 +0200

    Replace /etc/postfix by /usr/local/etc/postfix in config

--- start-postfix.sh
+++ start-postfix.sh
@@ -3,11 +3,11 @@
 # Copy default config to config dir
 if [ ! "$(ls -A $POSTFIX_CONF_DIR)" ]; then
     cp -r /etc/postfix/* $POSTFIX_CONF_DIR
+    find $POSTFIX_CONF_DIR -type f -exec sed -i 's/\/etc\/postfix/\/usr\/local\/etc\/postfix/g' {} \;
 fi
 
 # Set configuration from env vars
 while read -r conf; do
-    echo $conf
     postconf -c $POSTFIX_CONF_DIR "${conf:9}"
 done <<< `env|grep POSTCONF_`
 
